72b9ff812fa85c5e44c7c1bd13a5e3585d9715ef
yuanliwei <891789592@qq.com>
2019-02-19 12:52:17 +0800

    添加 TimePickerDialog.java

--- app/src/main/java/com/ylw/viewdemo/TimePickerDialog.java
+++ app/src/main/java/com/ylw/viewdemo/TimePickerDialog.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.contrarywind.adapter.WheelAdapter;
 import com.contrarywind.view.WheelView;
@@ -13,9 +14,13 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * 日期时间选择器
+ * <p>
+ * 选择器样式：<br/>
+ * <img src="http://192.168.0.34/AndroidGroup/up366TeacherV4/uploads/ebb1dd78bd224d1403db94692d57f16b/image.png"/>
  * <p>
  * Created by 袁立位 on 2019/2/18 15:50.
  */
@@ -28,17 +33,18 @@ public class TimePickerDialog {
     }
 
     /**
-     * 日期事件选择器
+     * 日期时间选择器
      */
     @SuppressLint("ClickableViewAccessibility")
     public void showMMDDHHmm(IOnSelectDateTime onSelectDateTime) {
         final BottomSheetDialog mBottomSheetDialog = new BottomSheetDialog(context);
-        View dialogView = context.getLayoutInflater().inflate(R.layout.dialog_time_picker, null);
+        View dialogView = context.getLayoutInflater().inflate(R.layout.dialog_time_picker_style_1, null);
         WheelView wheelView1 = dialogView.findViewById(R.id.wheel1);
         WheelView wheelView2 = dialogView.findViewById(R.id.wheel2);
         WheelView wheelView3 = dialogView.findViewById(R.id.wheel3);
         Button cancel = dialogView.findViewById(R.id.cancel);
         Button ok = dialogView.findViewById(R.id.ok);
+        TextView year = dialogView.findViewById(R.id.year);
         cancel.setOnClickListener(v -> mBottomSheetDialog.dismiss());
         ok.setOnClickListener(v -> {
             Calendar calendar = Calendar.getInstance();
@@ -59,8 +65,8 @@ public class TimePickerDialog {
         wheelView1.setOnTouchListener(touchHandler);
         wheelView2.setOnTouchListener(touchHandler);
         wheelView3.setOnTouchListener(touchHandler);
-        SimpleDateFormat format1 = new SimpleDateFormat("  MM月dd日");
-        SimpleDateFormat format2 = new SimpleDateFormat("  MM.dd");
+        SimpleDateFormat format1 = new SimpleDateFormat("  MM月dd日", Locale.getDefault());
+        SimpleDateFormat format2 = new SimpleDateFormat("  MM.dd", Locale.getDefault());
         WheelAdapter adapter1 = new WheelAdapter() {
             @Override
             public int getItemsCount() {
@@ -74,6 +80,7 @@ public class TimePickerDialog {
                 calendar.add(Calendar.DAY_OF_WEEK, index);
                 Date d = calendar.getTime();
                 if (index == wheelView1.getCurrentItem()) {
+                    year.setText(String.valueOf(calendar.get(Calendar.YEAR)));
                     return format1.format(d);
                 }
                 return format2.format(d);
